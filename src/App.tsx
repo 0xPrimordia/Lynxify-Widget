5e3b630f5a82f0fb1d67dd38366809d6fbe8b7fc
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-05-14 14:20:54 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import './polyfills';
+import { HashportWidget } from '@hashport/widget';
+
+function App() {
+    return (
+        <>
+            <h1>Lynxify Bridge</h1>
+            <HashportWidget />
+        </>
+    );
+}
+
+
+export default App
